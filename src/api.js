a62aba93d817a41ef3fe69808f7536658507c3d6
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2025-07-15 12:42:28 +0700

    update: endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://api.deadline-warrior.blog/api/",
 });
 
 API.interceptors.request.use((req) => {
